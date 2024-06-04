5deba43ec6daaf4a630a30211986b154896df8fa
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-05 00:56:41 +0600

    Solved

--- 2a.fsx
+++ 2a.fsx
@@ -1,37 +1,33 @@
 open System
 open System.Collections.Generic
+open System.Numerics
 
-// Dictionary to store previously computed Fibonacci numbers
-let dict = Dictionary<int, bigint>()
+let dict = Dictionary<int64, int64>()  // Use BigInteger for sum
 
 // Recursive Fibonacci function with memoization
-let rec fib n = 
+let rec fib n =
     match n with
-    | 0 -> 0I
-    | 1 -> 1I
+    | 0L -> 0L
+    | 1L -> 1L
     | _ ->
         if dict.ContainsKey(n) then
             dict.[n]
         else
-            let value = fib (n - 1) + fib (n - 2)
+            let value = fib(n - 1L) + fib(n - 2L)
             dict.[n] <- value
             value
 
-// Read number of test cases
-let t = Console.ReadLine() |> int 
+// [<EntryPoint>]
+let main args =
+    let t = Console.ReadLine() |> int64
+    for _ in 1L .. t do  // Loop for each test case
+        let n = Console.ReadLine() |> int64  // Read n for each case
 
-for _ in 1 .. t do
-    // Read input number
-    let n = Console.ReadLine() |> bigint.Parse
-    
-    let mutable input = 0
-    let mutable res = 0I
-    let mutable fibValue = fib input
-    while fibValue <= n do
-        if fibValue % 2I = 0I then 
-            res <- res + fibValue
-        input <- input + 1
-        fibValue <- fib input
-
-    // Print the result
-    printfn "%A" res
+        let sum = Seq.initInfinite (fun i -> fib(int64 i)) 
+                   |> Seq.filter (fun elm -> elm % 2L = 0L) 
+                   |> Seq.takeWhile (fun x -> x < n)
+                   |> Seq.fold (fun acc x -> acc + BigInteger(x)) BigInteger.Zero  // BigInteger sum
+        Console.WriteLine(sum)
+    0
+        
+main [||]
