8fa445c8ab46d3775fef83eb2de83b887ac2a7a0
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-12 13:12:16 +0600

    Some Improvements

--- 7a.fsx
+++ 7a.fsx
@@ -1,11 +1,23 @@
 open System
 
+exception NegativeNumberException of string
 
-let isPrime x = 
-    let checkingSeqn = {2..(int32(Math.Sqrt(float x)))}
+let checkIfNegativeInputInt (input: int)  :int = 
+    if input < 0 then
+        raise (NegativeNumberException "The int you have passed cannot be negative")
+    else
+        input
+
+
+let isPrime (x: int): bool = 
+    // mathematically guaranteed to be no primes after sqrt(x)
+    let checkingSeqn = {2..(int32(Math.Sqrt(float x)))} 
     checkingSeqn |> Seq.forall (fun it -> x % it <> 0)
 
-let rec generateNthPrime n candidate =
+// we check if the number is prime, if it is we decrement n and increment the candidate
+// if it is not we increment the candidate and check again
+// we are guaranteed to find the nth prime number when we hit base case n = 0
+let rec generateNthPrime (n: int) (candidate: int): int =
     match n with
     | 0 -> candidate 
     | _ -> 
@@ -16,11 +28,20 @@ let rec generateNthPrime n candidate =
             generateNthPrime n nextCandidate
 
 let main args = 
-    let t = Console.ReadLine() |> int
-    for i in 1..t do
-        let n = Console.ReadLine() |> int
-        let result = generateNthPrime n 1
-        Console.WriteLine(result)
+    try 
+        let testCaseCount = Console.ReadLine() |> int |> checkIfNegativeInputInt
+        for testCase in 1..testCaseCount do
+            let input = Console.ReadLine() |> int |> checkIfNegativeInputInt
+            let result = generateNthPrime input 1
+            Console.WriteLine(result)
+    with 
+        | :? System.FormatException -> printfn "Formatting error has occured"
+        | :? System.IO.IOException -> printfn "An IO exception occurred"
+        | :? System.OutOfMemoryException -> printfn "Out of memory"
+        | :? System.OverflowException -> printfn "Number is too large"
+        | :? NegativeNumberException as e -> printfn "%s" e.Message
+        | _ -> printfn "An error occurred"
+
     0
 
 main [| |]
