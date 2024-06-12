b6db64344aded8026d9ce37a47924906576a6c6e
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-12 17:08:21 +0600

    Improvements to readability

--- 12a.fsx
+++ 12a.fsx
@@ -1,30 +1,50 @@
 open System
 
+exception NegativeNumberException of string
+
+let checkIfNegativeInputInt (input: int)  :int = 
+    if input < 0 then
+        raise (NegativeNumberException "The int you have passed cannot be negative")
+    input
+
 // dictionary to store vals
 let dict = System.Collections.Generic.Dictionary<int, int>()
 
-let rec buildVal n = 
-    match n with 
+let rec buildValForTriangleNumbers (index: int) = 
+    match index with 
     | 1 -> 1
     | _ -> 
-        match dict.TryGetValue(n) with
+        match dict.TryGetValue(index) with
         | true, v -> v
         | _ -> 
             let v = 
-                buildVal (n - 1) + n 
-            dict.Add(n, v)
+                buildValForTriangleNumbers (index - 1) + index 
+            dict.Add(index, v)
             v
-let seqn = Seq.initInfinite( fun i -> buildVal (i + 1))
+let numberOfDivisors (input: int) = 
+    let divisorSeqn = {1..input}
+    divisorSeqn |> Seq.filter (fun divisor -> input % divisor  = 0) |> Seq.length
+let infiniteSequenceOfTriangleNumbers = Seq.initInfinite( fun (i: int) -> buildValForTriangleNumbers (i + 1))
 
-let numberOfDivisors n = 
-    let seqn = {1..n}
-    seqn |> Seq.filter (fun x -> n % x = 0) |> Seq.length
+let main args: unit =
+    try 
+        let testCaseCount = Console.ReadLine() |> int |> checkIfNegativeInputInt
+        for testCase in 1..testCaseCount do
+            let input = Console.ReadLine()
+                        |> int 
+                        |> checkIfNegativeInputInt
+            infiniteSequenceOfTriangleNumbers 
+                    |> Seq.filter (fun triangleNumber -> numberOfDivisors triangleNumber > input) 
+                    |> Seq.head 
+                    |> printfn "%d"
+    with
+    | :? System.FormatException -> printfn "Formatting error has occured"
+    | :? System.IO.IOException -> printfn "An IO exception occurred"
+    | :? System.OutOfMemoryException -> printfn "Out of memory"
+    | :? System.OverflowException -> printfn "Number is too large"
+    | :? NegativeNumberException as e -> printfn "%s" e.Message
+    | _ -> printfn "An error occurred"
 
-let main args = 
-    let t = Console.ReadLine() |> int
-    for _ in 1..t do
-        let n = Console.ReadLine() |> int
-        seqn |> Seq.filter (fun x -> numberOfDivisors x > n) |> Seq.head |> printfn "%d"
-    0
+    
 
 main [| |]
