9ea9cbc8eaef57934a2eb6d762d904de96b9bfb3
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-12 12:17:10 +0600

    Some improvements

--- 3a.fsx
+++ 3a.fsx
@@ -1,22 +1,41 @@
 open System
 open System.Numerics
 
-let largestPrimeFactor (n: bigint) =
-    let mutable n = n
-    let mutable i = 2I
-    let mutable largest = 0I
-    while i * i <= n do
-        while n % i = 0I do
-            largest <- i
-            n <- n / i
-        i <- i + 1I
-    if n > 1I then
-        largest <- n
-    largest
+exception NegativeNumberException of string
 
-let t = Console.ReadLine() |> int
+let largestPrimeFactor (input: bigint) = 
+    let isPrime (input: bigint) = 
+        let checkingNumbers: BigInteger seq = seq {2I..bigint(sqrt(double(input)))}
+        checkingNumbers |> Seq.forall (fun number -> input % number <> 0I)
+    
+    let candidateNumbers: BigInteger seq = seq {2I..input}
+    candidateNumbers |> Seq.filter (fun number -> input % number = 0I && isPrime number) |> Seq.max
 
-for _ in 1 .. t do
-    let n = Console.ReadLine() |> BigInteger.Parse
-    let result = largestPrimeFactor n
-    printfn "%A" result
+
+let checkIfNegativeInputInt (input: int)  :int = 
+    if input < 0 then
+        raise (NegativeNumberException "The int you have passed cannot be negative")
+    input
+
+let checkIfNegativeInputBigInt (input:bigint) :bigint =
+    if input < 0I then
+        raise (NegativeNumberException "The bigint you have passed cannot be negative")
+    input
+
+
+try 
+    let testCaseCount: int = Console.ReadLine() |> int |>  checkIfNegativeInputInt
+    
+
+    for testCase in 1 .. testCaseCount do
+        let input = Console.ReadLine() |> BigInteger.Parse  |> checkIfNegativeInputBigInt
+        let result = largestPrimeFactor input
+        printfn "%A" result
+with
+    | :? System.FormatException -> printfn "Formatting error has occured"
+    | :? System.IO.IOException -> printfn "An IO exception occurred"
+    | :? System.OutOfMemoryException -> printfn "Out of memory"
+    | :? System.OverflowException -> printfn "Number is too large"
+    | :? NegativeNumberException as e -> printfn "%s" e.Message
+    | _ -> printfn "An error occurred"
+
