5339fb71c1410b0a66179477602c2c8f42be352d
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-12 17:29:40 +0600

    Improvements

---
+++ 16a.fsx
@@ -0,0 +1,18 @@
+open System
+
+
+let main args =
+    let t  = Console.ReadLine() |> int
+    for _ in 1..t do
+        let n = Console.ReadLine() |> int
+        let constant = bigint.Parse("2")
+        let res = bigint.Pow(constant, n)
+        let resStr = res.ToString()
+        let sum = resStr.ToCharArray() |> Array.map (fun c -> int32 c - int32 '0') |> Array.sum |> int
+        printfn "%d" sum
+             
+    0
+
+main [| |]
+
+
