4fb6bf18e6004b5caae3cd5f7e7063389a847cc6
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-12 13:02:45 +0600

    Further Improvements

--- 6a.fsx
+++ 6a.fsx
@@ -1,19 +1,41 @@
 open System
 open System.Numerics
 
+exception NegativeNumberException of string
 
-let t = Console.ReadLine() |> BigInteger.Parse
 
-for _ in 1I .. t do
-    let n = Console.ReadLine() |> BigInteger.Parse
-    // let val1 = [1I..n] |> List.map (fun x -> x*x) |> List.sum 
-    // let val2 = [1I..n] |> List.sum |> fun x -> x*x
-    // formula method 
-    let val1 = (n * (n + 1I) * (2I * n + 1I)) / 6I
-    let val2 = ((n * (n + 1I)) / 2I) * ((n * (n + 1I)) / 2I)
-    
-    let res = abs (val2 - val1)
+let checkIfNegativeInputBigInt (input:bigint) :bigint =
+    if input < 0I then
+        raise (NegativeNumberException "The bigint you have passed cannot be negative")
+    input
 
-    printfn "%A" res
+
+let solution (input: BigInteger): BigInteger = 
+    // the mathematical formulas have been checked for correctness
+    let sumOfSquares (input: BigInteger): BigInteger = 
+        (input * (input + 1I) * (2I * input + 1I)) / 6I
+
+    let squareOfSum (input: BigInteger): BigInteger = 
+        ((input * (input + 1I)) / 2I) * ((input * (input + 1I)) / 2I)
+
+    abs (sumOfSquares input - squareOfSum input)
+
+let testCaseCount = Console.ReadLine() |> BigInteger.Parse |> checkIfNegativeInputBigInt
+
+for testCase in 1I .. testCaseCount do
+    try 
+        let input: bigint = Console.ReadLine() |> BigInteger.Parse |> checkIfNegativeInputBigInt
+        let res: BigInteger = solution input
+        printfn "%A" res
+    with
+    | :? System.FormatException -> printfn "Formatting error has occured"
+    | :? System.IO.IOException -> printfn "An IO exception occurred"
+    | :? System.OutOfMemoryException -> printfn "Out of memory"
+    | :? System.OverflowException -> printfn "Number is too large"
+    | :? NegativeNumberException as e -> printfn "%s" e.Message
+    | _ -> printfn "An error occurred"
+
+
+
     
 
