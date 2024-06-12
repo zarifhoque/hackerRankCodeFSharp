e1e2fb7bcc2c38eacc0458c7cfd27b700861d03f
Zarif Safwan Hoque <hoquelabid@gmail.com>
2024-06-12 11:40:38 +0600

    Some changes

--- 1a.fsx
+++ 1a.fsx
@@ -1,9 +1,11 @@
 open System
 
 
+exception NegativeNumberException of string
+
 let sumOfMultipleOfThreeOrFive (endingTerm: int64) : int64 = 
     let sumationOfAUptoN (endingTerm: int64) (startingTerm: int64) : int64  =
-        let numberOfTerms : int64 = ((endingTerm-1L)/startingTerm)
+        let numberOfTerms: int64 = ((endingTerm-1L)/startingTerm)
         startingTerm * numberOfTerms * (numberOfTerms + 1L) / 2L
 
     let sumOf3:  int64 = sumationOfAUptoN endingTerm 3L
@@ -13,19 +15,23 @@ let sumOfMultipleOfThreeOrFive (endingTerm: int64) : int64 =
 
 let main argv =
     try 
-        let numberOfTestCases = Console.ReadLine() |> int64
+        let numberOfTestCases = Console.ReadLine()|> int64
+        if numberOfTestCases < 1L then
+            raise (NegativeNumberException "Invalid input")
         for testCase in 1L .. numberOfTestCases do
             try 
                 let input = Console.ReadLine() |> int64
+                if input < 1L then
+                    raise (NegativeNumberException "Invalid input")
                 let result = sumOfMultipleOfThreeOrFive (int64 input)
                 printfn "%d" result
-            with 
-                | :? System.FormatException -> printfn "Invalid input"
-                | :? System.OverflowException -> printfn "Invalid input"
+         
 
     with 
     | :? System.FormatException -> printfn "Invalid input"
     | :? System.OverflowException -> printfn "Invalid input"
+    | :? NegativeNumberException as ex -> printfn "%s" ex.Message
+    | _ -> printfn "An error occurred"
 
     0 // return an integer exit code
 
